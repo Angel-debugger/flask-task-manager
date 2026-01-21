ebf1ce8f7565992b8ee1e877c8b8b0a0058187a6
Angela.Sales <Angela.Sales@Telemabos-iMac-2015.local>
2026-01-21 17:08:05 +0100

    Fix SQLite instance path for Render

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/tasks.db'
+
+os.makedirs(app.instance_path, exist_ok=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -54,4 +58,4 @@ def delete_task(task_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run()
